3365cf5dcce591e6e564df30885897c4ba2e1b03
ririan <113106114+RIANXPPLG1@users.noreply.github.com>
2023-09-04 15:03:49 +0700

    Add files via upload

---
+++ coba/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$connect = mysqli_connect('localhost','root','','db_sekolah');
+echo mysqli_error($connect);
+$query = mysqli_query($connect,'SELECT * FROM tb_siswa');
+
+$rows = [];
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$test = mysqli_fetch_array($query);
+var_dump($test['NAMA_SISWA']);
+
+?>
+    
+</body>
+</html>
